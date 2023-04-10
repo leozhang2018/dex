f80ee2254300fdf9869dabe2f08ca7df53fb6c42
leozhang2018 <leozhang2018@gmail.com>
2023-04-10 14:57:39 +0800

    feat:add zadig-x login template.
    Signed-off-by: leozhang2018 <leozhang2018@gmail.com>

--- custom-web/static/js/main.js
+++ custom-web/static/js/main.js
@@ -7,60 +7,77 @@ var dex = new Vue({
     el: "#dex-login",
     delimiters: ["<<<", ">>>"],
     data: {
+        loading: false,
         loginForm: {
             account: "",
             password: "",
         },
-        showRegistration:false,
-        showLoginError:false,
-        showPassword:false
+        otherLogin:{
+            login:'',
+            password:''
+        },
+        showRegistration: false,
+        showLoginError: false,
+        showPassword: false,
+        rules: {
+            account: [
+                { required: true, message: '请输入用户名', trigger: 'change' }
+            ],
+            password: [{ required: true, message: '请输入密码', trigger: 'change' }]
+        }
     },
-    computed:{
-        year(){
+    computed: {
+        year() {
             return new Date().getFullYear()
         }
     },
     methods: {
         async login() {
-            const payload = this.loginForm;
-            try {
-                const userInfoRes = await axios.post("/api/v1/login", payload);
-                const userInfo = userInfoRes.data;
-                if (userInfo) {
-                    store.set("userInfo", userInfo);
-                    const roleBindingRes = await axios.get(
-                        `/api/v1/userbindings?uid=${userInfo.uid}`,
-                        {
-                            headers: {
-                                Authorization: `Bearer ${userInfo.token}`,
-                            },
-                        }
-                    );
-                    const roleBinding = roleBindingRes.data;
-                    if (roleBinding) {
-                        const role = roleBinding.map((item) => item.role);
-                        store.set("role", role);
-                        if(port){
-                            window.location.href = `${protocol}//${hostname}:${port}/v1/projects`;
-                        }
-                        else{
-                            window.location.href = `${protocol}//${hostname}/v1/projects`;
+            this.$refs.loginForm.validate(async valid => {
+                if (valid) {
+                    const payload = this.loginForm;
+                    this.loading = true
+                    try {
+                        const userInfoRes = await axios.post("/api/v1/login", payload);
+                        const userInfo = userInfoRes.data;
+                        if (userInfo) {
+                            store.set("userInfo", userInfo);
+                            const roleBindingRes = await axios.get(
+                                `/api/v1/userbindings?uid=${userInfo.uid}`,
+                                {
+                                    headers: {
+                                        Authorization: `Bearer ${userInfo.token}`,
+                                    },
+                                }
+                            );
+                            const roleBinding = roleBindingRes.data;
+                            if (roleBinding) {
+                                const role = roleBinding.map((item) => item.role);
+                                store.set("role", role);
+                                if (port) {
+                                    window.location.href = `${protocol}//${hostname}:${port}/v1/projects`;
+                                }
+                                else {
+                                    window.location.href = `${protocol}//${hostname}/v1/projects`;
+                                }
+                            }
                         }
                     }
+                    catch (error) {
+                        this.loading = false
+                        this.showLoginError = true;
+                    }
                 }
-            } 
-            catch (error) {
-                this.showLoginError = true;
-            }
+            })
 
         },
-        async registrationCheck(){
+        async registrationCheck() {
             const registrationRes = await axios.get(`/api/v1/features/RegisterTrigger`);
             if (registrationRes.data && registrationRes.data.enabled) {
                 this.showRegistration = true
-              } else {
+            } else {
                 this.showRegistration = false
-              }
+            }
         }
     },
     mounted() {
